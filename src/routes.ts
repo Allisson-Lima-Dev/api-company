0762daf3a4258bf0d56933c2cf05533d5681458e
Allisson Lima <Allysoncosta@outlook.com>
2022-08-25 15:51:42 -0300

    feat: config .env

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,25 @@
 import { Router } from "express";
-
+interface user {
+  name: string;
+  age: number;
+}
 const router = Router();
 
 router.post("/projects", (req, res) => {
-  return res.status(201).send();
+  try {
+    const { name, age } = req.body as user;
+
+    if (name === "Allisson") {
+      res.status(200).send("Seja bem vindo meu Guerreiro");
+    } else {
+      res.status(404).send("não encontradp");
+    }
+  } catch (error) {
+    res.send(error);
+  }
+});
+router.get("/", (req, res) => {
+  return res.status(200).send("Conseguiu guerreiro");
 });
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+require("dotenv").config();
 import { app } from "./app";
 
-app.listen(3333, () => console.log("Server is running 🚀"));
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log("Server is running 🚀"));
